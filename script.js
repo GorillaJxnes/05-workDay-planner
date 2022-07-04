14b5ce699f54169bb308428b7cc20e9b5a5badd0
Gorilla Jones <kgorski90lol@gmail.com>
2022-07-04 17:54:25 -0500

    final push

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ $("#hour-9 .description").val(localStorage.getItem("hour-9"))
 $("#hour-10 .description").val(localStorage.getItem("hour-10"))
 $("#hour-11 .description").val(localStorage.getItem("hour-11"))
 $("#hour-12 .description").val(localStorage.getItem("hour-12"))
-$("#hour-1 .description").val(localStorage.getItem("hour-1"))
-$("#hour-2 .description").val(localStorage.getItem("hour-2"))
-$("#hour-3 .description").val(localStorage.getItem("hour-3"))
-$("#hour-4 .description").val(localStorage.getItem("hour-4"))
-$("#hour-5 .description").val(localStorage.getItem("hour-5"))
+$("#hour-13 .description").val(localStorage.getItem("hour-13"))
+$("#hour-14 .description").val(localStorage.getItem("hour-14"))
+$("#hour-15.description").val(localStorage.getItem("hour-15"))
+$("#hour-16 .description").val(localStorage.getItem("hour-16"))
+$("#hour-17 .description").val(localStorage.getItem("hour-17"))
     $(".saveBtn").on("click", function(){
         var value = $(this).siblings('.description').val();
         var time = $(this).parent().attr("id");
@@ -20,23 +20,26 @@ $("#hour-5 .description").val(localStorage.getItem("hour-5"))
     })
     function updater(){
         var currentTime = moment().hours();
-            $('.time-block').each(function(){
-                var blockHour = parseInt($(this).attr("#currentDay").split("-")[1])
+            $(".time-block").each(function(){
+                var blockHour = this.id.split("-")[1];
                 if(blockHour < currentTime){
-                    $(this).removeClass("#future");
-                    $(this).removeClass("#present");
-                    $(this).addClass("#past");
+                    $(this).removeClass("future");
+                    $(this).removeClass("present");
+                    $(this).addClass("past");
                 }
                 else if(blockHour === currentTime){
-                    $(this).removeClass("#past");
-                    $(this).removeClass("#present")
-                    $(this).addClass("#future")
+                    $(this).removeClass("past");
+                    $(this).removeClass("present")
+                    $(this).addClass("future")
                 }
                 else {
-                    $(this).removeClass("#past")
-                    $(this).removeClass("#present")
-                    $(this).addClass("#future")
+                    $(this).removeClass("past")
+                    $(this).removeClass("present")
+                    $(this).addClass("future")
                 }
             })
-    }
+            
+    }updater();
     })
+
+    
